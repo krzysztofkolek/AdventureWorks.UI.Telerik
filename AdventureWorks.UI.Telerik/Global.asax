ae57c41a58209d29d518814a1a771c4699ee2456
krzysztof.kolek.praca <krzysztof.kolek.praca@gmail.com>
2016-06-12 15:11:08 +0200

    Initial commit - default project There will be no Telerik assets commitet!!

---
+++ AdventureWorks.UI.Telerik/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdventureWorks.UI.Telerik.MvcApplication" Language="C#" %>
---
+++ AdventureWorks.UI.TelerikTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventureWorks.UI.TelerikTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
