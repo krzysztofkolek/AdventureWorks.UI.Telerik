7aa48d274fc531905db86f6668a6922aa9cb39f3
krzysztofkolek <krzysztof.kolek.praca@gmail.com>
2016-07-22 15:23:30 +0200

    WPF template - Telerik mail

---
+++ AdventureWorks.UI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Telerik.Windows.Controls;
+
+namespace AdventureWorks.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : RadRibbonWindow
+    {
+        static MainWindow()
+        {
+            RadRibbonWindow.IsWindowsThemeEnabled = false;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            IconSources.ChangeIconsSet(IconsSet.Modern);
+        }
+    }
+}
---
+++ AdventureWorks.UI.WPF/Views/MailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AdventureWorks.UI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MailView.xaml
+    /// </summary>
+    public partial class MailView : UserControl
+    {
+        public MailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
